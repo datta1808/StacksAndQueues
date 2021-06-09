492961e537b3bf35fba2c71a5f9bec23aee5f371
datta1808 <bagaledatta18@gmail.com>
2021-06-10 01:51:58 +0530

    Welcome to Stacks and Queues

---
+++ src/test/java/com/bridgelabz/MyLinkedListTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+
+    @Test
+    public void ifStackIsCreated_shouldReturnTrue() {
+        Stack<Integer> stack = new Stack<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        stack.push(myThirdNode);
+        stack.push(mySecondNode);
+        stack.push(myFirstNode);
+        INode peek = stack.peek();
+        Assert.assertEquals(peek, myFirstNode);
+    }
+}
+
