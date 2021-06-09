e22e29ac5de4b056296b275b010e62d5a0d3c798
datta1808 <bagaledatta18@gmail.com>
2021-06-10 02:25:42 +0530

    ADDED : Functionality to create a Queue

--- src/test/java/com/bridgelabz/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/MyLinkedListTest.java
@@ -30,5 +30,18 @@ public class MyLinkedListTest {
         INode popStack = stack.pop();
         Assert.assertEquals(popStack, myFirstNode);
     }
+
+    @Test
+    public void IfQueueIsCreatedShouldReturnTrue() {
+        Queue<Integer> queue = new Queue<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        queue.enqueue(myFirstNode);
+        queue.enqueue(mySecondNode);
+        queue.enqueue(myThirdNode);
+        INode peek = queue.peek();
+        Assert.assertEquals(peek, myFirstNode);
+    }
 }
 
